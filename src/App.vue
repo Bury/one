d7adddd93c7ddbb46e8be5dc35997b54984b3c87
lantian <lantian@qq.com>
2018-09-05 11:36:35 +0800

    lantian,

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,6 @@
     <div id="app">
       <router-view></router-view>
     </div>
-    <div id="imageBg">
-      <div class="top">
-        <div class="top-left">
-          <img src="./assets/images/logo.png"/>
-          <img src="./assets/images/logoname.png"/>
-        </div>
-        <div class="top-middle"></div>
-        <div class="top-right"></div>
-      </div>
-
-      <div class="content">
-        <div class="content-left">
-          <div class="content-left-top"></div>
-          <div class="content-left-middle"></div>
-          <div class="content-left-bottom"></div>
-        </div>
-        <div class="content-middle">
-          <div class="content-middle-top"></div>
-          <div class="content-middle-middle"></div>
-          <div class="content-middle-bottom"></div>
-        </div>
-        <div class="content-right">
-          <div class="content-right-top"></div>
-          <div class="content-right-bottom"></div>
-        </div>
-      </div>
-    </div>
   </div>
 
 </template>
@@ -94,42 +67,5 @@ ul,li{list-style: none;}
 .el-message .el-message__icon{
 	margin: 0 10px;
 }
-  #imageBg{
-    background: url("./assets/images/bg.png") center center no-repeat;
-    background-size: cover;
-    width: 100%;
-    padding: 15px 25px;
-    overflow: hidden;
-  }
-  .top,.content{
-    display: flex;
-    overflow: hidden;
-  }
-  .top-middle{
-    width: 67rem;
-    height: 4.6rem;
-    background: rgba(149,199,255,0.5);
-  }
-  .top-right{
-    width: 18rem;
-    background: #95c7ff;
-    opacity: 0.5;
-  }
-  .content{
-    width: 100%;
-    height:48.1rem;
-    margin-top: 2rem;
-  }
-.content-left{
-  width:47rem;
-  background: rgba(149,199,255,0.5);
-}
-.content-middle{
-  width:30rem;
-  background: rgba(149,199,255,0.5);
-}
-.content-right{
-  width:25rem;
-  background: rgba(149,199,255,0.5);
-}
+ 
 </style>
